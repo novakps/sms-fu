2fe5e992109945f30099952d07b9041e18d303df
Douglas A. Seifert <doug@dseifert.net>
2012-08-07 06:35:57 -0700

    Don't die if rails is not available

--- lib/sms_fu.rb
+++ lib/sms_fu.rb
@@ -14,4 +14,8 @@ require 'sms_fu/sms_fu'
 require 'sms_fu/sms_fu_helper'
 require 'sms_fu/sms_notifier'
 
-require 'sms_fu/rails'
+begin
+  require 'sms_fu/rails'
+rescue Exception => e
+  puts "sms_fu: WARN: rails not available, unable to initialize Rails engine"
+end
